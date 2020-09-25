9cdbb30aa9f621712410d824e08ac1c06ba72adb
bonifacyj <katyasumina@google.com>
2020-09-25 08:53:39 +0000

    Implement rudimentary metadata_scraper and synchronizer

---
+++ google-datacatalog-kafka-connector/src/google/datacatalog_connectors/kafka/scrape/metadata_scraper.py
@@ -0,0 +1,31 @@
+import logging
+from confluent_kafka import Consumer
+
+
+class MetadataScraper:
+
+    def __init__(self):
+        pass
+
+    def get_metadata(self, connection_args):
+        metadata = self._get_metadata_from_message_broker_connection(
+            connection_args)
+        metadata = self._create_metadata_dict(metadata)
+        return metadata
+
+    def _get_metadata_from_message_broker_connection(self, bootstrap_servers):
+        consumer = Consumer({'bootstrap.servers': bootstrap_servers})
+        try:
+            cluster_metadata = consumer.list_topics()
+            topic_names = cluster_metadata.topics.keys()
+            return topic_names
+        except:  # noqa:E722
+            logging.error(
+                'Error connecting to the system to extract metadata.')
+            raise
+        finally:
+            consumer.close()
+
+    def _create_metadata_dict(self, metadata):
+        dict_metadata = {'topics': metadata}
+        return dict_metadata
