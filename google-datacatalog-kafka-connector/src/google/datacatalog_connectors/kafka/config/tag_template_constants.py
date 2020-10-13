f63931d1b8cfa0b9570bac0270a6c5d61137a007
bonifacyj <katyasumina@google.com>
2020-10-13 11:10:22 +0000

    Refactor TagFactory to use centrally defined constants

--- google-datacatalog-kafka-connector/src/google/datacatalog_connectors/kafka/config/tag_template_constants.py
+++ google-datacatalog-kafka-connector/src/google/datacatalog_connectors/kafka/config/tag_template_constants.py
@@ -11,6 +11,8 @@ class TagTemplateConstants:
     """
 
     def __init__(self):
+        self.cluster_fields = None
+        self.topic_fields = None
         self.field_constants_for_cluster = self.\
             _define_constants_for_cluster_tag_template()
         self.field_constants_for_topic = self.\
@@ -24,6 +26,27 @@ class TagTemplateConstants:
             self.type = field_type
             self.is_required = is_required
 
+    class ClusterFields:
+
+        def __init__(self, num_brokers, num_topics, bootstrap_address):
+            self.num_brokers = num_brokers
+            self.num_topics = num_topics
+            self.bootstrap_address = bootstrap_address
+
+    class TopicFields:
+
+        def __init__(self, num_partitions, retention_time, retention_space,
+                     min_compaction_lag, max_compaction_lag, cleanup_policy,
+                     consumer_groups, schema):
+            self.num_partitions = num_partitions
+            self.retention_time = retention_time
+            self.retention_space = retention_space
+            self.min_compaction_lag = min_compaction_lag
+            self.max_compaction_lag = max_compaction_lag
+            self.cleanup_policy = cleanup_policy
+            self.consumer_groups = consumer_groups
+            self.schema = schema
+
     def _define_constants_for_cluster_tag_template(self):
         num_brokers = self.TagTemplateFieldConstants(
             'num_brokers', 'Number of brokers',
@@ -34,6 +57,8 @@ class TagTemplateConstants:
         bootstrap_address = self.TagTemplateFieldConstants(
             'bootstrap_address', 'Bootstrap address',
             datacatalog_v1beta1.enums.FieldType.PrimitiveType.STRING, True)
+        self.cluster_fields = self.ClusterFields(num_brokers, num_topics,
+                                                 bootstrap_address)
         return [num_brokers, num_topics, bootstrap_address]
 
     def _define_constants_for_topic_tag_template(self):
@@ -61,6 +86,12 @@ class TagTemplateConstants:
         schema = self.TagTemplateFieldConstants(
             'schema', 'Schema',
             datacatalog_v1beta1.enums.FieldType.PrimitiveType.STRING)
+        self.topic_fields = self.TopicFields(num_partitions, retention_time,
+                                             retention_space,
+                                             min_compaction_lag,
+                                             max_compaction_lag,
+                                             cleanup_policy, consumer_groups,
+                                             schema)
         topic_tag_template_fields = [
             num_partitions, retention_time, retention_space,
             min_compaction_lag, max_compaction_lag, cleanup_policy,
--- google-datacatalog-kafka-connector/src/google/datacatalog_connectors/kafka/prepare/datacatalog_tag_factory.py
+++ google-datacatalog-kafka-connector/src/google/datacatalog_connectors/kafka/prepare/datacatalog_tag_factory.py
@@ -1,42 +1,52 @@
 from google.cloud import datacatalog_v1beta1
-from google.datacatalog_connectors.kafka.config import MetadataConstants
+from google.datacatalog_connectors.kafka.config import MetadataConstants, TagTemplateConstants
 
 
 class DataCatalogTagFactory:
 
     def __init__(self):
-        pass
+        self._tag_template_constants = TagTemplateConstants()
 
     def make_tag_for_cluster(self, tag_template, cluster_metadata):
         tag = datacatalog_v1beta1.types.Tag()
         tag.template = tag_template.name
-        tag.fields['num_brokers'].double_value = cluster_metadata[
-            MetadataConstants.NUM_BROKERS]
-        tag.fields['bootstrap_address'].string_value = cluster_metadata[
-            MetadataConstants.BOOTSTRAP_SERVER]
+        template_fields = self._tag_template_constants.cluster_fields
+        tag.fields[
+            template_fields.num_brokers.name].double_value = cluster_metadata[
+                MetadataConstants.NUM_BROKERS]
+        tag.fields[template_fields.bootstrap_address.
+                   name].string_value = cluster_metadata[
+                       MetadataConstants.BOOTSTRAP_SERVER]
         num_topics = len(cluster_metadata[MetadataConstants.TOPICS])
-        tag.fields['num_topics'].double_value = num_topics
+        tag.fields[template_fields.num_topics.name].double_value = num_topics
         return tag
 
     def make_tag_for_topic(self, tag_template, topic_metadata):
         tag = datacatalog_v1beta1.types.Tag()
         tag.template = tag_template.name
-        tag.fields['num_partitions'].double_value = topic_metadata[
-            MetadataConstants.NUM_PARTITIONS]
-        tag.fields['cleanup_policy'].string_value = topic_metadata[
-            MetadataConstants.CLEANUP_POLICY]
+        template_fields = self._tag_template_constants.topic_fields
+        tag.fields[
+            template_fields.num_partitions.name].double_value = topic_metadata[
+                MetadataConstants.NUM_PARTITIONS]
+        tag.fields[
+            template_fields.cleanup_policy.name].string_value = topic_metadata[
+                MetadataConstants.CLEANUP_POLICY]
         retention_time = topic_metadata.get(MetadataConstants.RETENTION_TIME)
         if retention_time:
-            tag.fields['retention_time'].string_value = retention_time
+            tag.fields[template_fields.retention_time.
+                       name].string_value = retention_time
         retention_space = topic_metadata.get(MetadataConstants.RETENTION_SPACE)
         if retention_space:
-            tag.fields['retention_space'].double_value = retention_space
+            tag.fields[template_fields.retention_space.
+                       name].double_value = retention_space
         min_compaction_lag = topic_metadata.get(
             MetadataConstants.MIN_COMPACTION_LAG)
         if min_compaction_lag:
-            tag.fields['min_compaction_lag'].string_value = min_compaction_lag
+            tag.fields[template_fields.min_compaction_lag.
+                       name].string_value = min_compaction_lag
         max_compaction_lag = topic_metadata.get(
             MetadataConstants.MAX_COMPACTION_LAG)
         if max_compaction_lag:
-            tag.fields['max_compaction_lag'].string_value = max_compaction_lag
+            tag.fields[template_fields.max_compaction_lag.
+                       name].string_value = max_compaction_lag
         return tag
