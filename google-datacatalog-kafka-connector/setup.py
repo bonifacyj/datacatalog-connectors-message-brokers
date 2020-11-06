419a947a4fcb5587670692ec98a7db649c76cd26
Katya Sumina <katyasumina@google.com>
2020-11-06 18:21:27 +0000

    Change pandas version to avoid dependenty conflict

--- google-datacatalog-kafka-connector/setup.py
+++ google-datacatalog-kafka-connector/setup.py
@@ -36,7 +36,7 @@ setuptools.setup(
         ],
     },
     include_package_data=True,
-    install_requires=('pandas>=1.1.4,<1.2.0', 'gcsfs',
+    install_requires=('pandas==0.24.2', 'gcsfs',
                       'google-datacatalog-connectors-commons', 'pyYAML',
                       'confluent-kafka', 'fastavro', 'avro'),
     setup_requires=('pytest-runner'),
